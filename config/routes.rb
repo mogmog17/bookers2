18adf9bdf177c8fb4ac6e62f17188af802b74b24
mogmog17 <hinalongze@gmail.com>
2022-07-14 11:19:06 +0000

    [Update]ファイルの記述・CRUDの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root to: "homes#top"
+  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:new, :index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20220714022647_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+
+      t.string :title #本のタイトル
+      t.string :body #感想
+      t.integer :user_id #userテーブルのidとアソシエーションしてuserテーブルの中身呼び出せる
+
+      t.timestamps
+    end
+  end
+end
