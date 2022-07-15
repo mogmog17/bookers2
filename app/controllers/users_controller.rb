2ab0537587cf94b3bae954f894f6e3ef090bf685
EC2 Default User <ec2-user@ip-172-31-58-66.ec2.internal>
2022-07-15 15:16:41 +0000

    [Update]log inページの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:title, :body, :image)
   end
 end
