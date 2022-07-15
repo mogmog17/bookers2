bdf2ff9a5f23dae417599b98c3c0c93c0e39c0a3
mogmog17 <hinalongze@gmail.com>
2022-07-15 15:16:41 +0000

    [Update]log inページの編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as:'about'
-  resources :users, only: [:index, :show, :edit]
-  resources :books, only: [:new, :index, :show, :edit]
+  post 'books' => 'books#create'
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:new, :index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
