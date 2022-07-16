49bfacf9f6c7e8deb1e934aeed4cc7c294812d7c
EC2 Default User <ec2-user@ip-172-31-58-66.ec2.internal>
2022-07-16 12:50:38 +0000

    [Update]部分テンプレート

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :books, dependent: :destroy #1:N の「1」側にあたるモデルに、has_many,「dependent: :destroy」を付けて実装
 
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/NoImage.jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
