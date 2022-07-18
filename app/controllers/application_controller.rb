20864e7010357bad4454b7dad383b893ee1442d5
EC2 Default User <ec2-user@ip-172-31-58-66.ec2.internal>
2022-07-18 12:46:44 +0000

    [Finish]課題３

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!, except: [:top, :about] #ログイン認証が済んでいない状態でトップページ以外にアクセスしても、ログイン画面へリダイレクト
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   #サインイン後の遷移先
   def after_sign_in_path_for(resource)
-    books_path
+    user_path(current_user)
   end
 
   #サインアウト後の遷移先
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,29 +9,45 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id) #新規投稿したらbookのshowページに遷移
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id) #新規投稿したらbookのshowページに遷移
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
     @user = @book.user
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id) #投稿の更新したらbookのshowページに遷移
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id]) #データ（レコード）を1件取得
     book.destroy #データ（レコード）を削除
-    redirect_to user_path(book.user.id) #ユーザーの詳細画面へリダイレクト
+    redirect_to books_path #ユーザーの詳細画面へリダイレクト
   end
 
 
